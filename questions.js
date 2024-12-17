6bc570a6999d63b1af082e23d99dc7b1a05fd3b4
DarsheelCodes <darsheelalai@gmail.com>
2024-12-17 10:02:07 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "If 2x - 5 = 15, then the value of x is:",
+        answer: "10",
+        options: [
+            "5",
+            "10",
+            "15",
+            "20"
+        ]
+    },
+    {
+        numb: 2,
+        question: "If 15 workers can finish a job in 20 days, how many workers are required to finish the same job in 12 days?",
+        answer: "25",
+        options: [
+            "10",
+            "15",
+            "20",
+            "25"
+        ]
+    },
+    {
+        numb: 3,
+        question: "A shopkeeper bought a table for Rs 800 and sold it for Rs 1000. What is his profit percentage?",
+        answer: "25%",
+        options: [
+            "20%",
+            "25%",
+            "30%",
+            "35%"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The compound interest on Rs 5000 at 10% per annum for 2 years, compounded annually, is:",
+        answer: "Rs. 1050",
+        options: [
+            "Rs. 1000",
+            "Rs. 1050",
+            "Rs. 1100",
+            "Rs. 1155"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The area of a trapezium with parallel sides of lengths 12 cm and 20 cm and a height of 10 cm is:",
+        answer: "160 cm²",
+        options: [
+            "160 cm²",
+            "180 cm²",
+            "200 cm²",
+            "220 cm²"
+        ]
+    },
+];
